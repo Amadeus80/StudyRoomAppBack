cca4582f7f3ca7fc36191bfda5b02b1f67958936
Pablo Acosta <acostaortizpablo@gmail.com>
2023-05-13 16:54:46 +0200

    Url de origenes permitidos desde variable de entorno en el application.properties

--- src/main/java/com/studyroom/studyroomapp/WebConfig.java
+++ src/main/java/com/studyroom/studyroomapp/WebConfig.java
@@ -1,5 +1,6 @@
 package com.studyroom.studyroomapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${allowed.url}")
+    private String urlAllowed;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        System.out.println(urlAllowed);
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
+            .allowedOrigins(urlAllowed)
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true)
